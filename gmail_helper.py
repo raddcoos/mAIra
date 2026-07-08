23ed5e27621603122450799511a5361810605e70
raddcoos <muhammadharithredza@gmail.com>
2026-07-08 12:15:38 +0800

    edit gmail_helper.py

--- gmail_helper.py
+++ gmail_helper.py
@@ -1,18 +1,18 @@
 import os
-import base64
+import smtplib
 from email.message import EmailMessage
-from googleapiclient.discovery import build
-from google.oauth2.credentials import Credentials
 
 def send_gmail(to_email, subject, body):
-    # This assumes you have a token.json in your directory
-    creds = Credentials.from_authorized_user_file('token.json', ['https://www.googleapis.com/auth/gmail.send'])
-    service = build('gmail', 'v1', credentials=creds)
-    
-    message = EmailMessage()
-    message.set_content(body)
-    message['To'] = to_email
-    message['Subject'] = subject
-    
-    raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-    service.users().messages().send(userId="me", body={'raw': raw_message}).execute()
+    email_user = os.getenv("EMAIL_USER")
+    email_pass = os.getenv("EMAIL_PASS")
+
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['Subject'] = subject
+    msg['From'] = email_user
+    msg['To'] = to_email
+
+    # Connect to Google's SMTP server
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_user, email_pass)
+        smtp.send_message(msg)
